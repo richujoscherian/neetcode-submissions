8794fa000b54de031117f784bc90d70d41051a06
richujoscherian <dev.richujoscherian@gmail.com>
2026-09-06 12:58:35 +0530

    Add: height-checker - submission-1

---
+++ Data Structures & Algorithms/height-checker/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        count=[0]*101
+        for h in heights: 
+            count[h]+=1
+        expected=[]
+        for h in range(len(count)):
+            if count[h] >= 1:
+                for i in range(count[h]):
+                    expected.append(h)
+        result=0            
+        for i in range(len(heights)):
+            if heights[i]!=expected[i]:
+                result+=1
+        return result        
+
+
