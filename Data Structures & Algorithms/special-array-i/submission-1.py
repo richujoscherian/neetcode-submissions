0f9a62e830cde1d35d830cf49d1cf56d5fec1568
richujoscherian <dev.richujoscherian@gmail.com>
2026-09-08 16:40:24 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(1,len(nums)):
+            div1=nums[i]%2
+            div2=nums[i-1]%2
+            if div1==div2:
+                return False
+        return True        
+
